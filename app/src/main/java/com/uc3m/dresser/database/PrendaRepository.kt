ab151663ad6e3218e72d6a42ce7eec4b49fda03d
Marcostp98 <37538168+Marcostp98@users.noreply.github.com>
2021-02-26 10:32:23 +0100

    prenda

--- app/src/main/java/com/uc3m/dresser/database/PrendaRepository.kt
+++ app/src/main/java/com/uc3m/dresser/database/PrendaRepository.kt
@@ -6,11 +6,8 @@ class PrendaRepository (private val prendaDao: PrendaDao) {
 
     val readAll: LiveData<List<Prenda>> = prendaDao.readAll()
 
-    suspend fun addUser (prenda: Prenda){
+    suspend fun addPrenda (prenda: Prenda){
         prendaDao.addPrenda(prenda)
     }
 
-    fun addPrenda(prenda: Prenda) {
-
-    }
 }
